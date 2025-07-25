22ff98f88974d50011ef3588f3b8d15cfaa1be33
TobiasSkog <t_ott_e@hotmail.com>
2025-07-26 01:54:50 +0200

    tihi

--- go.mod
+++ go.mod
@@ -5,15 +5,19 @@ go 1.24.3
 require (
 	github.com/bwmarrin/dgvoice v0.0.0-20210225172318-caaac756e02e
 	github.com/bwmarrin/discordgo v0.29.0
+	github.com/disgoorg/disgo v0.18.16
+	github.com/disgoorg/snowflake/v2 v2.0.3
 	github.com/joho/godotenv v1.5.1
+	github.com/lrstanley/go-ytdlp v1.1.1
 	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32
 )
 
 require (
 	github.com/ProtonMail/go-crypto v1.3.0 // indirect
 	github.com/cloudflare/circl v1.6.1 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/lrstanley/go-ytdlp v1.1.1 // indirect
+	github.com/disgoorg/json v1.2.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad // indirect
 	github.com/ulikunitz/xz v0.5.12 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,47 @@
+// var (
+// token string = os.Getenv("DISCORD_TOKEN")
+// channelIDstring string = os.Getenv("DISCORD_CHANNEL_ID")
+// guildIDstring   string = os.Getenv("DISCORD_GUILD_ID")
+// channelIDraw, _              = strconv.ParseUint(os.Getenv("DISCORD_CHANNEL_ID"), 10, 64)
+// guildIDraw, _                = strconv.ParseUint(os.Getenv("DISCORD_GUILD_ID"), 10, 64)
+// channelID       snowflake.ID = snowflake.ID(channelIDraw)
+// guildID         snowflake.ID = snowflake.ID(guildIDraw)
+// )
+//var guilds map[string]*Guild
+
 package main
 
 import (
-	// "main/dgvoice"
-	// "main/ytdlp"
-
-	// "fmt"
-	"log"
+	"context"
+	"encoding/binary"
+	"io"
+	"log/slog"
 	"os"
 	"os/signal"
-	// "strings"
+	"strconv"
+	"syscall"
+	"time"
 
-	"github.com/bwmarrin/discordgo"
+	"log"
+
+	"github.com/disgoorg/disgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/gateway"
+	"github.com/disgoorg/disgo/voice"
+	"github.com/disgoorg/snowflake/v2"
+
 	"github.com/joho/godotenv"
 )
 
-var guilds map[string]*Guild
+// var token string = os.Getenv("DISCORD_TOKEN")
+// var channelIDraw, _ = strconv.ParseUint(os.Getenv("DISCORD_CHANNEL_ID"), 10, 64)
+// var guildIDraw, _ = strconv.ParseUint(os.Getenv("DISCORD_GUILD_ID"), 10, 64)
+// var channelID snowflake.ID = snowflake.ID(channelIDraw)
+// var guildID snowflake.ID = snowflake.ID(guildIDraw)
+// print("token: ", token)
+// print("\nguildID: ", guildID)
+// print("\nchannelID: ", channelID)
 
 func main() {
 	err := godotenv.Load()
@@ -23,49 +50,101 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	Token := os.Getenv("DISCORD_TOKEN")
-	//Prefix := os.Getenv("COMMAND_PREFIX")
+	var token string = os.Getenv("DISCORD_TOKEN")
+	print("token: ", token)
+	slog.Info("starting up")
+	slog.Info("disgo version", slog.String("version", disgo.Version))
 
-	session, _ := discordgo.New("Bot " + Token)
+	s := make(chan os.Signal, 1)
 
-	guilds = make(map[string]*Guild)
-
-	session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-		log.Printf("Logged in as %s", r.User.String())
-	})
-	session.AddHandler(messageCreate)
-	session.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsDirectMessages | discordgo.IntentsGuildVoiceStates | discordgo.IntentsGuilds
-	session.StateEnabled = true
-
-	// Start
-	err = session.Open()
+	client, err := disgo.New(token,
+		bot.WithGatewayConfigOpts(gateway.WithIntents(gateway.IntentGuildVoiceStates)),
+		bot.WithEventListenerFunc(func(e *events.Ready) {
+			go play(e.Client(), s)
+		}),
+	)
 	if err != nil {
-		log.Fatalf("could not open session: %s", err)
+		slog.Error("error creating client", slog.Any("err", err))
 	}
 
+	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		client.Close(ctx)
+	}()
 
-	// Gracefully shutdown with Ctrl + C
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
-	<-stop
-	err = session.Close()
-	if err != nil {
-		log.Printf("could not close session gracefully: %s", err)
-	}
-}
-
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore messages sent by the bot itself
-	if m.Author.ID == s.State.User.ID {
+	if err = client.OpenGateway(context.TODO()); err != nil {
+		slog.Error("error connecting to gateway", slog.Any("error", err))
 		return
 	}
 
-	// If guild exists, pass message. If not, create a new one and handle message
-	if guild, exists := guilds[m.GuildID]; exists {
-		guild.HandleMessage(s, m)
-	} else {
-		newGuild := NewGuild(m.GuildID, "-p")
-		guilds[m.GuildID] = newGuild
-		newGuild.HandleMessage(s, m)
+	slog.Info("ExampleBot is now running. Press CTRL-C to exit.")
+	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-s
+}
+
+func play(client bot.Client, closeChan chan os.Signal) {
+	var channelIDraw, _ = strconv.ParseUint(os.Getenv("DISCORD_CHANNEL_ID"), 10, 64)
+	var guildIDraw, _ = strconv.ParseUint(os.Getenv("DISCORD_GUILD_ID"), 10, 64)
+	var channelID snowflake.ID = snowflake.ID(channelIDraw)
+	var guildID snowflake.ID = snowflake.ID(guildIDraw)
+
+	var channelIDraw2, _ = strconv.ParseUint(os.Getenv("DISCORD_CHANNEL_ID2"), 10, 64)
+	var guildIDraw2, _ = strconv.ParseUint(os.Getenv("DISCORD_GUILD_ID2"), 10, 64)
+	var channelID2 snowflake.ID = snowflake.ID(channelIDraw)
+	var guildID2 snowflake.ID = snowflake.ID(guildIDraw)
+
+	conn := client.VoiceManager().CreateConn(guildID)
+	conn := client.VoiceManager().CreateConn(guildID2)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	if err := conn.Open(ctx, channelID, false, false); err != nil {
+		panic("error connecting to voice channel: " + err.Error())
+	}
+	defer func() {
+		closeCtx, closeCancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer closeCancel()
+		conn.Close(closeCtx)
+	}()
+
+	if err := conn.SetSpeaking(ctx, voice.SpeakingFlagMicrophone); err != nil {
+		panic("error setting speaking flag: " + err.Error())
+	}
+	for i := 1; i < 20; i++ {
+		writeOpus(conn.UDP())
+	}
+	closeChan <- syscall.SIGTERM
+}
+
+func writeOpus(w io.Writer) {
+	file, err := os.Open("nico.dca")
+	if err != nil {
+		panic("error opening file: " + err.Error())
+	}
+	ticker := time.NewTicker(time.Millisecond * 20)
+	defer ticker.Stop()
+
+	var lenBuf [4]byte
+	for range ticker.C {
+		_, err = io.ReadFull(file, lenBuf[:])
+		if err != nil {
+			if err == io.EOF {
+				_ = file.Close()
+				return
+			}
+			panic("error reading file: " + err.Error())
+			return
+		}
+
+		// Read the integer
+		frameLen := int64(binary.LittleEndian.Uint32(lenBuf[:]))
+
+		// Copy the frame.
+		_, err = io.CopyN(w, file, frameLen)
+		if err != nil && err != io.EOF {
+			_ = file.Close()
+			return
+		}
 	}
 }
